da6972fbda0ba9a341b2ffd2d548cd62896bc5ba
Brandon Brobst <brandondbrobst@gmail.com>
2021-06-19 01:53:33 -0500

    begin index view table

--- DepthChartManager/Controllers/HomeController.cs
+++ DepthChartManager/Controllers/HomeController.cs
@@ -6,21 +6,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace DepthChartManager.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand command = new SqlCommand();
+        SqlDataReader dataReader;
+        SqlConnection connection = new SqlConnection();
+
+        List<PlayerInfo> rosterInfo = new List<PlayerInfo>();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            connection.ConnectionString = DepthChartManager.Properties.Resources.ConnectionString;
         }
 
         public IActionResult Index()
         {
-            return View();
+            FetchData();
+            return View(rosterInfo);
         }
 
         public IActionResult Privacy()
@@ -28,6 +37,41 @@ namespace DepthChartManager.Controllers
             return View();
         }
 
+        private void FetchData()
+        {
+            if(rosterInfo.Count > 0)
+            {
+                rosterInfo.Clear();
+            }
+            try
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT TOP (5) [playerID],[cfbdapiID],[firstName],[lastName],[jerseyNumber],[position],[pos_rank],[height],[weight],[yearsExperience],[isActive] FROM [DepthChartManager].[dbo].[Player]";
+                dataReader = command.ExecuteReader();
+                while(dataReader.Read())
+                {
+                    Trace.WriteLine(dataReader["lastName"].ToString());
+                    rosterInfo.Add(new PlayerInfo() { PlayerID = (int)dataReader["playerID"],
+                                                      CfbdapiID = (int)dataReader["cfbdapiID"],
+                                                      FirstName = dataReader["firstName"].ToString(),
+                                                      LastName = dataReader["lastName"].ToString(),
+                                                      JerseyNumber = (int)dataReader["jerseyNumber"],
+                                                      Position = dataReader["position"].ToString(),
+                                                      PosRank = dataReader["pos_rank"] as int? ?? default,
+                                                      Height = (int)dataReader["height"],
+                                                      Weight = (int)dataReader["weight"],
+                                                      YearsExperience = dataReader["yearsExperience"] as int? ?? default,
+                                                      IsActive = dataReader["isActive"] as int? ?? default
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ DepthChartManager/Models/PlayerInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepthChartManager.Models
+{
+    public class PlayerInfo
+    {
+        public int PlayerID { get; set; }
+        public int CfbdapiID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int JerseyNumber { get; set; }
+        public string Position { get; set; }
+
+        public int PosRank { get; set;}
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        public int YearsExperience { get; set; }
+        public int IsActive { get; set; }
+    }
+}
